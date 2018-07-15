bb318c1c1ae027d431671aa6875e9528de3b2188
Joe Hall <joefhall@gmail.com>
2018-07-15 19:36:13 +0000

    Add data

---
+++ data/fake-news.js
@@ -0,0 +1,30 @@
+fakeNews = { 
+  popularity: {
+    2008: 3,
+    2009: 3,
+    2010: 2,
+    2011: 2,
+    2012: 1,
+    2013: 3,
+    2014: 3,
+    2015: 3,
+    2016: 27,
+    2017: 100
+   },
+  
+  grants: {
+    2008: [ { org: 'ALL', amount: 0 } ],
+    2009: [ { org: 'ALL', amount: 0 }, ],
+    2010: [ { org: 'ALL', amount: 0 }, ],
+    2011: [ { org: 'ALL', amount: 0 }, ],
+    2012: [ { org: 'ALL', amount: 0 }, ],
+    2013: [ { org: 'ALL', amount: 0 }, ],
+    2014: [ { org: 'ALL', amount: 0 }, ],
+    2015: [ { org: 'ALL', amount: 0 }, ],
+    2016: [ { org: 'ALL', amount: 0 }, ],
+    2017: [
+      { org: 'ALL', amount: 124000 },
+      { org: 'The Wellcome Trust', amount: 124000 },
+    ]
+  }
+};
---
+++ js/data.js
@@ -0,0 +1,33 @@
+data = {};
+funders = [];
+startYear = 2008;
+endYear = 2017;
+
+themes = {
+  fakeNews: 'Fake news',
+  genderEquality: 'Gender equality',
+  mentalHealth: 'Mental health',
+  transgender: 'Transgender'
+};
+
+function getAllFunders() {
+  for (var theme in data) {
+    for (var year = startYear; year <= endYear; year++) {
+      for (var i = 0; i < data[theme]['grants'][year].length; i++) {
+        if (data[theme]['grants'][year][i]['org'] !== 'ALL' && $.inArray(data[theme]['grants'][year][i]['org'], funders) === -1) {
+          funders.push(data[theme]['grants'][year][i]['org']);
+        }
+      }
+    }
+  }
+}
+
+
+
+$(document).ready(function() {
+  for (var theme in themes) {
+    data[theme] = window[theme];
+  }
+  
+  getAllFunders();
+});
